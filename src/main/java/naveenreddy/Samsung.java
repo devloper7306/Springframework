600360d9d6cf36d996bec36dd5ce04d89230f947
devloper7306 <devops.usa7306@gmail.com.com>
2022-12-21 19:14:59 -0500

    new commit

--- src/main/java/naveenreddy/Samsung.java
+++ src/main/java/naveenreddy/Samsung.java
@@ -1,11 +1,10 @@
 package naveenreddy;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Samsung {
-@Bean	
+
 public void config() {
 	System.out.println("Octa Core, 4gb Ram, 12MP Camera");
 }
